f79340888f218210a83db5fe676bb90e7adadba8
🐼🐼 <camchin1@msn.cn>
2022-05-31 13:48:00 +0800

    熊凯：upgrade at 2022-05-31 13:48:00

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module wasm/apkparse
 
 go 1.18
 
-require github.com/shogo82148/androidbinary v1.0.3
+// require github.com/shogo82148/androidbinary v1.0.3
--- main.go
+++ main.go
@@ -1,24 +1,71 @@
 package main
 
 import (
-	"log"
-
-	"github.com/shogo82148/androidbinary"
-	"github.com/shogo82148/androidbinary/apk"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync"
+	"syscall/js"
 )
 
-func main() {
-	pkg, _ := apk.OpenFile("app.apk")
-	defer pkg.Close()
-	icon, _ := pkg.Icon(nil)
-	pkgName := pkg.PackageName()
-	resConfigEN := &androidbinary.ResTableConfig{
-		Language: [2]uint8{uint8('z'), uint8('h')},
-	}
-	// appLabel :=
-	appLabel, _ := pkg.Label(resConfigEN)
+var wg sync.WaitGroup
 
-	log.Printf("  icon %q...", icon)
-	log.Printf("  pkgName %q...", pkgName)
-	log.Printf("  appLabel %q...", appLabel)
+func main() {
+	js.Global().Set("formatJSON", jsonWrapper())
+	js.Global().Set("post", js.FuncOf(post))
+	js.Global().Set("hello", js.FuncOf(Hello))
+
+	<-make(chan bool)
+}
+func Hello(this js.Value, args []js.Value) interface{} {
+	message := args[0].String() // get the parameters
+	return "Hello " + message
+}
+
+func post(this js.Value, args []js.Value) interface{} {
+	url := args[0].String() // get the parameters
+	fmt.Println(url)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return "error"
+	}
+	resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+	fmt.Println(resp.StatusCode)
+	if resp.StatusCode == 200 {
+		fmt.Println("ok")
+	}
+	return resp
+}
+
+func jsonWrapper() js.Func {
+	jsonFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		if len(args) != 1 {
+			return "Invalid no of arguments passed"
+		}
+		inputJSON := args[0].String()
+		fmt.Printf("input %s\n", inputJSON)
+		pretty, err := prettyJson(inputJSON)
+		if err != nil {
+			fmt.Printf("unable to convert to json %s\n", err)
+			return err.Error()
+		}
+		return pretty
+	})
+	return jsonFunc
+}
+
+func prettyJson(input string) (string, error) {
+	var raw interface{}
+	if err := json.Unmarshal([]byte(input), &raw); err != nil {
+		return "", err
+	}
+	pretty, err := json.MarshalIndent(raw, "", "  ")
+	if err != nil {
+		return "", err
+	}
+	return string(pretty), nil
 }
